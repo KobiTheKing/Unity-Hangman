4ba47d696ed8022972063c7f7c8631f630a6543f
KobiTheKing <46636253+KobiTheKing@users.noreply.github.com>
2019-01-13 22:26:03 -0500

    Added Start Menu

---
+++ Hangman/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/*
+ * Description: Controls the start menu
+ * Methods: void Awake(), void TypeWord(), void RandomWord()
+ */
+public class StartMenu : MonoBehaviour {
+
+    //Access 'Word' script
+    private Word word;
+
+    /*
+     * Used for initialization
+     */
+    private void Awake() {
+        word = GameObject.FindObjectOfType<Word>();
+    }
+
+    /*
+     * Description: Called of the type word button is clicked. Allows the user to chose the word for the hangman game
+     */
+    public void TypeWord() {
+        Word.RandomizeWord = false;
+        Word.InGetWordScene = true;
+
+        SceneManager.LoadScene("GetWord");
+    }
+
+    /*
+     * Description: Called of the random word button is clicked. Randomly chooses the word for the hangman game
+     */
+    public void RandomWord() {
+        Word.RandomizeWord = true;
+        Word.InGetWordScene = true;
+
+        SceneManager.LoadScene("GetWord");
+    }
+
+}
---
+++ Hangman/Assets/Scripts/Word.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Description: Generates and stores the word being guessed
+ * Methods: void Awake(), void Start(), void TypeWord()
+ */
+public class Word : MonoBehaviour {
+
+    //True if randomizing word, false if typing word
+    private static bool randomizeWord;
+
+    //True if in the 'GetWord' scene
+    private static bool inGetWordScene;
+
+    /*
+     * Description: Used for initialization
+     */
+    void Awake() {
+        //Default values
+        RandomizeWord = true;
+        InGetWordScene = false;
+    }
+
+    /*
+     * Description: Called when object is created (after 'Awake')
+     */
+    private void Start() {
+        if (InGetWordScene) {
+            if (RandomizeWord) {
+                RandomWord();
+            } else {
+                TypeWord();
+            }
+        }
+    }
+
+    /*
+     * Description: Allows the user to type in a word for the game
+     */
+    private void TypeWord() {
+        Debug.Log("type");
+    }
+
+    /*
+     * Description: Randomly generated a word for the game
+     */
+    private void RandomWord() {
+        Debug.Log("random");
+    }
+
+    public static bool RandomizeWord { get => randomizeWord; set => randomizeWord = value; }
+    public static bool InGetWordScene { get => inGetWordScene; set => inGetWordScene = value; }
+}

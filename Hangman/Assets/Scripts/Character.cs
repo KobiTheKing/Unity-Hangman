b58ecd19ffd811ff2dbe03e04884917a5c92cdf9
Kobi <kobif2001@gmail.com>
2019-01-26 17:49:24 -0500

    develop - Add Character.cs

--- Hangman/Assets/Scripts/Character.cs
+++ Hangman/Assets/Scripts/Character.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetupGame : MonoBehaviour {
+public class Character : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
